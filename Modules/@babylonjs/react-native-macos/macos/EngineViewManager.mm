ad6f264d92f402ad40d63b26cc7525180afa47a3
Jihoo Byeon <jihoobyeon@gmail.com>
2023-07-29 21:09:53 +0900

    Tidy up EngineViewManager.mm

--- Modules/@babylonjs/react-native-macos/macos/EngineViewManager.mm
+++ Modules/@babylonjs/react-native-macos/macos/EngineViewManager.mm
@@ -22,10 +22,10 @@
 }
 
 - (instancetype)init:(RCTBridge*)_bridge {
-	#ifdef TARGET_OS_OSX
-		const CGSize monitor = [[NSScreen mainScreen] frame].size;
-		const CGSize appWin = [[NSApp mainWindow] frame].size;
-    if (self = [super initWithFrame:CGRectMake((appWin.width - monitor.width) / 2, (appWin.height - monitor.height) / 2, monitor.width, monitor.height)
+  #ifdef TARGET_OS_OSX
+        const CGSize monitor = [[NSScreen mainScreen] frame].size;
+        const CGSize appWin = [[NSApp mainWindow] frame].size;
+        if (self = [super initWithFrame:CGRectMake((appWin.width - monitor.width) / 2, (appWin.height - monitor.height) / 2, monitor.width, monitor.height)
 			device:MTLCreateSystemDefaultDevice()]) {
         bridge = _bridge;
         super.translatesAutoresizingMaskIntoConstraints = false;
@@ -33,20 +33,20 @@
         super.depthStencilPixelFormat = MTLPixelFormatDepth32Float;
     }
     
-		[self setBounds:CGRectMake((appWin.width - monitor.width) / 2, (appWin.height - monitor.height) / 2, monitor.width, monitor.height)];
-	#else
-		if (self = [super initWithFrame:CGRectZero device:MTLCreateSystemDefaultDevice()]) {
+	[self setBounds:CGRectMake((appWin.width - monitor.width) / 2, (appWin.height - monitor.height) / 2, monitor.width, monitor.height)];
+  #else
+	if (self = [super initWithFrame:CGRectZero device:MTLCreateSystemDefaultDevice()]) {
         bridge = _bridge;
         super.translatesAutoresizingMaskIntoConstraints = false;
         super.colorPixelFormat = MTLPixelFormatBGRA8Unorm_sRGB;
         super.depthStencilPixelFormat = MTLPixelFormatDepth32Float;
     }
-	#endif
+  #endif
     return self;
 }
 
 - (void)setIsTransparentFlag:(NSNumber*)isTransparentFlag {
-	#ifndef TARGET_OS_OSX
+  #ifndef TARGET_OS_OSX
     BOOL isTransparent = [isTransparentFlag intValue] == 1;
     if(isTransparent){
         [self setOpaque:NO];
@@ -54,7 +54,7 @@
         [self setOpaque:YES];
     }
     self.isTransparent = isTransparent;
-	#endif
+  #endif
 }
 
 - (void)setMSAA:(NSNumber*)value {
@@ -99,7 +99,7 @@
         [xrView removeFromSuperview];
         xrView = nil;
     }
-	#endif
+  #endif
     [BabylonNativeInterop renderView];
 }
 
@@ -108,7 +108,7 @@
 }
 
 - (void)takeSnapshot {
-	#ifndef TARGET_OS_OSX
+  #ifndef TARGET_OS_OSX
     // We must take the screenshot on the main thread otherwise we might fail to get a valid handle on the view's image.
     dispatch_async(dispatch_get_main_queue(), ^{
         // Start the graphics context.
@@ -128,7 +128,7 @@
             self.onSnapshotDataReturned(@{ @"data":encodedData});
         }
     });
-	#endif
+  #endif
 }
 
 #ifdef TARGET_OS_OSX
